9d14752d36b5ea1edef706a40568d1b869afbc65
Stefan <stgrybo4@web.de>
2023-08-30 10:24:31 +0200

    Implemetierung mit der  Klasse AsciiRechteck

--- 03_Properties_Rechteck.py
+++ 03_Properties_Rechteck.py
@@ -4,11 +4,11 @@ class Rechteck:
         self._b = b
 
     @property
-    def seite(self):
+    def seiten(self):
         return self._a, self._b
 
-    @seite.setter
-    def seite(self, seitenlaengen):
+    @seiten.setter
+    def seiten(self, seitenlaengen):
         a, b = seitenlaengen
         if a <= 0 or b <= 0:
             print("Die Seitenlänge muss größer als 0 sein!")
@@ -27,12 +27,36 @@ print("Der Umfang beträgt:", mein_rechteck.umfang(), "cm")
 print("Der Flächeninhalt beträgt:", mein_rechteck.flaecheninhalt(), "cm²")
 
 
-mein_rechteck.seite = (10, 10)
+mein_rechteck.seiten = (10, 10)
 print("Der Umfang beträgt:", mein_rechteck.umfang(), "cm")
 print("Der Flächeninhalt beträgt:", mein_rechteck.flaecheninhalt(), "cm²")
 
 
-mein_rechteck.seite = (-2, 0)
+mein_rechteck.seiten = (-2, 0)
 print("Der Umfang beträgt:", mein_rechteck.umfang(), "cm")
 print("Der Flächeninhalt beträgt:", mein_rechteck.flaecheninhalt(), "cm²")
+
+class AsciiRechteck(Rechteck):
+
+    def __init__(self, a, b, zeichen):
+        super().__init__(a, b)
+        self._zeichen = zeichen
+
+    @property
+    def zeichen(self):
+        return self._zeichen
+
+    @zeichen.setter
+    def zeichen(self, zeichen):
+        if len(zeichen) == 1:
+            self._zeichen = zeichen
+        else:
+            print("Das Zeichen muss genau ein Zeichen lang sein!")
+
+    def zeichne(self):
+        for i in range(self._b):
+            print(self._zeichen * self._a)
+
+mein_ascii_rechteck = AsciiRechteck(5, 5, ")")
+mein_ascii_rechteck.zeichne()
 
