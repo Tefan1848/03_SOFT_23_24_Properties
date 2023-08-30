1128971992495aa5fcc7ca27aa88b48df9753281
Stefan <stgrybo4@web.de>
2023-08-30 09:36:15 +0200

    Second Programm with Properties

---
+++ 02_Properties.py
@@ -0,0 +1,11 @@
+class Konto:
+    def __init__(self, gk, tg):
+        self.girokonto = gk
+        self.tagesgeld = tg
+
+    @property
+    def kontostand(self):
+        return self.girokonto + self.tagesgeld
+
+mein_konto = Konto(800, 50)
+print(mein_konto.kontostand)
